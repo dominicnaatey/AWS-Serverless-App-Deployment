10b722f7d48c8226ac15c25be685288c42d054a0
Dom7k <dominicnaatey@gmail.com>
2024-05-08 00:52:36 +0000

    app.js updated

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-// Add your API endpoint here
-var API_ENDPOINT = "https://9qsm0u8nmb.execute-api.us-west-2.amazonaws.com/dep";
+$(document).ready(function(){
+    var API_ENDPOINT = "https://bf7h44b347.execute-api.us-west-2.amazonaws.com/Test";
 
-
-// AJAX POST request to save employee data
-document.getElementById("saveEmployee").onclick = function(){
-    var inputData = {
-        "employeeid": $('#employeeid').val(),
-        "name": $('#name').val(),
-        "role": $('#role').val(),
-        "age": $('#age').val()
-    };
-    $.ajax({
-        url: API_ENDPOINT,
-        type: 'POST',
-        data:  JSON.stringify(inputData),
-        contentType: 'application/json; charset=utf-8',
-        success: function (response) {
-            document.getElementById("employeeSaved").innerHTML = "Employee Data Saved!";
-        },
-        error: function () {
-            alert("Error saving employee data.");
-        }
+    // AJAX POST request to save employee data
+    $("#saveemployee").click(function(){
+        var inputData = {
+            "employeeid": $('#employeeid').val(),
+            "name": $('#name').val(),
+            "role": $('#role').val(),
+            "age": $('#age').val()
+        };
+        $.ajax({
+            url: API_ENDPOINT,
+            type: 'POST',
+            data:  JSON.stringify(inputData),
+            contentType: 'application/json; charset=utf-8',
+            success: function (response) {
+                $("#employeeSaved").text("Employee Data Saved!");
+            },
+            error: function () {
+                alert("Error saving employee data.");
+            }
+        });
     });
-}
 
-// AJAX GET request to retrieve all employees
-document.getElementById("getEmployees").onclick = function(){  
-    $.ajax({
-        url: API_ENDPOINT,
-        type: 'GET',
-        contentType: 'application/json; charset=utf-8',
-        success: function (response) {
-            $('#employeeTable tr').slice(1).remove();
-            jQuery.each(response, function(i, data) {          
-                $("#employeeTable").append("<tr> \
-                    <td>" + data['employeeid'] + "</td> \
-                    <td>" + data['name'] + "</td> \
-                    <td>" + data['role'] + "</td> \
-                    <td>" + data['age'] + "</td> \
-                    </tr>");
-            });
-        },
-        error: function () {
-            alert("Error retrieving employee data.");
-        }
+    // AJAX GET request to retrieve all employees
+    $("#getemployees").click(function(){  
+        $.ajax({
+            url: API_ENDPOINT,
+            type: 'GET',
+            contentType: 'application/json; charset=utf-8',
+            success: function (response) {
+                $('#employeeTable tbody').empty();
+                $.each(response, function(i, data) {          
+                    $("#employeeTable tbody").append("<tr> \
+                        <td>" + data['employeeid'] + "</td> \
+                        <td>" + data['name'] + "</td> \
+                        <td>" + data['role'] + "</td> \
+                        <td>" + data['age'] + "</td> \
+                        </tr>");
+                });
+            },
+            error: function () {
+                alert("Error retrieving employee data.");
+            }
+        });
     });
-}
+});
